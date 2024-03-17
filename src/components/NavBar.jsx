94c9a311c14525cde291e0908930483b8a9c8e26
IbraheemKhaleel <khaleelibraheem05@gmail.com>
2024-03-17 14:02:52 +0530

    1. added cart count to navbar 2. added check button to navbar 3. added checkout form

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,14 @@
-export default function NavBar({ user } = false) {
+export default function NavBar({ user, cartItems, onCheckoutClick }) {
   return (
     <>
-      {user ? (
-        <span>{`Welcome ${user.firstName} ${user.lastName} `}</span>
-      ) : (
-        <a href="#">Login </a>
-      )}
-      <span>
-        {user && user.itemsInCart !== 0 && `${user.itemsInCart} in your cart`}
-      </span>
-      <a href="#home">Home </a>
-      <a href="#home">Laptops </a>
-      <a href="#contact">Contact </a>
-      <a href="#about">About </a>
+      <span>{`Welcome ${user.firstName} ${user.lastName}`}</span>
+      <a href="#">Login</a>
+      <a href="#home">Home</a>
+      <a href="#home">Laptops</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+      <button onClick={onCheckoutClick}>Checkout</button>
+      <span>{`${cartItems} in your cart`}</span>
     </>
   );
 }
